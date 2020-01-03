be97255bfc864fead27ae71120d2eaae76c5c8ee
Drew <drewclementsdesign@gmail.com>
2020-01-02 20:40:30 -0500

    small cleanup/rebuild

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import NuxtLoading from './components/nuxt-loading.vue'
-import NuxtBuildIndicator from './components/nuxt-build-indicator'
 
 import '../assets/css/tailwind.css'
 
@@ -40,7 +39,7 @@ export default {
       domProps: {
         id: '__nuxt'
       }
-    }, [loadingEl, h(NuxtBuildIndicator), transitionEl])
+    }, [loadingEl, transitionEl])
   },
   data: () => ({
     isOnline: true,
@@ -99,8 +98,6 @@ export default {
     },
 
     setLayout(layout) {
-      if(layout && typeof layout !== 'string') throw new Error('[nuxt] Avoid using non-string value as layout property.')
-
       if (!layout || !layouts['_' + layout]) {
         layout = 'default'
       }
